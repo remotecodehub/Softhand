9a5487d834e7319dd45f3b69a570e0688f28a816
weslleyluiz <weslleyluiz@outlook.com.br>
2023-12-06 15:37:06 -0300

    changed messenger to weakreferencemessenger

---
+++ Softhand/Messages/EditBuddyMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+using pjsua2xamarin.pjsua2;
+
+namespace Softhand.Messages
+{
+	public class EditBuddyMessage : ValueChangedMessage<BuddyConfig>
+    {
+		public EditBuddyMessage(BuddyConfig buddyConfig) : base (buddyConfig)
+		{
+		}
+	}
+}
+
