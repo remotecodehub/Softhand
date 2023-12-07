4865c5d267b136723d1ae461483d1089b6ca350c
weslleyluiz <weslleyluiz@outlook.com.br>
2023-12-06 22:09:31 -0300

    Added jni libs and csharp bindings for android

---
+++ Softhand/Usings.cs
@@ -0,0 +1,29 @@
+﻿//#if __IOS__
+//global using org.pjsip.pjsua2.iOS;
+//#endif
+//#if __ANDROID__
+//global using org.pjsip.pjsua2.Android;
+//#endif
+
+
+
+
+global using CommunityToolkit.Mvvm.Messaging;
+global using CommunityToolkit.Mvvm.Messaging.Messages;
+
+global using Microsoft.Extensions.Logging;
+
+global using pjsua2xamarin.pjsua2;
+
+global using Softhand.Messages;
+global using Softhand.Models;
+global using Softhand.Views;
+global using Softhand.ViewModels;
+global using Softhand.Handlers;
+
+
+
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Runtime.CompilerServices;
+
