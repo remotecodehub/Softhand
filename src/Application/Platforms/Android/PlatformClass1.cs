93bb7c862f2583ce041a4770826dfe96362e5510
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-09-08 15:18:22 -0300

    deleted layers projects since the pjsip sdk doesnt works on ios targets into the layers projects

--- src/Application/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application
-{
-    // All the code in this file is only included on Android.
-    public class PlatformClass1
-    {
-    }
-}
--- src/Domain/Platforms/Tizen/PlatformClass1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Domain
-{
-    // All the code in this file is only included on Tizen.
-    public class PlatformClass1
-    {
-    }
-}
