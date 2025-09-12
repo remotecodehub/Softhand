a154bf68d1ea90e1372d25a9f641d296a1a8966e
Weslley Murdock <weslleyluiz@outlook.com.br>
2025-09-12 00:31:29 -0300

    docs

--- src/Softhand/Platforms/iOS/Program.cs
+++ src/Softhand/Platforms/iOS/Program.cs
@@ -1,12 +1,25 @@
-﻿using ObjCRuntime;
+﻿#if IOS
+using ObjCRuntime;
 using UIKit;
 
+#pragma warning disable IDE0130 // O namespace não corresponde à estrutura da pasta
 namespace Softhand;
+#pragma warning restore IDE0130 // O namespace não corresponde à estrutura da pasta
 
+/// <summary>
+/// Entrypoint of ios application
+/// </summary>
 public class Program
 {
-	// This is the main entry point of the application.
-	static void Main(string[] args)
+    /// <summary>
+    /// protected ctor to avoid turn Program class static
+    /// </summary>
+    protected Program()
+    {
+        
+    }
+    // This is the main entry point of the application.
+    static void Main(string[] args)
 	{
 		// if you want to use a different Application Delegate class from "AppDelegate"
 		// you can specify it here.
@@ -14,3 +27,4 @@ public class Program
 	}
 }
 
+#endif
